7f85adcc796cff1db608ba61527fecd53ca8f9bc
Abhishek <abhi@dev.com>
2025-11-08 18:37:20 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/jbms/JbmsApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.jbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbmsApplication.class, args);
+	}
+
+}
