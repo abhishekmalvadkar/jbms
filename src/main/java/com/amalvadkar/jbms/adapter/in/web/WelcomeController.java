a38fc235fa441a418125ff592c33fa30e06a1d9b
Abhishek <abhi@dev.com>
2025-11-08 22:54:39 +0530

    Created endpoint to show welcome page with required details on website visit

--- src/main/java/com/amalvadkar/jbms/adapter/in/web/WelcomeController.java
+++ src/main/java/com/amalvadkar/jbms/adapter/in/web/WelcomeController.java
@@ -1,13 +1,17 @@
 package com.amalvadkar.jbms.adapter.in.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping("/")
-    public String welcome(){
+    public String welcome(Model model){
+        model.addAttribute("welcomeMessage", "Welcome to JBMS");
+        model.addAttribute("totalBookmarksCount", 2);
+        model.addAttribute("totalTagsCount", 3);
         return "welcome";
     }
 
--- src/test/java/com/amalvadkar/jbms/adapter/in/web/WelcomeControllerMvcTest.java
+++ src/test/java/com/amalvadkar/jbms/adapter/in/web/WelcomeControllerMvcTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@WebMvcTest
+@WebMvcTest(WelcomeController.class)
 public class WelcomeControllerMvcTest {
 
     @Autowired
@@ -20,5 +24,20 @@ public class WelcomeControllerMvcTest {
                 .getResponse()
                 .getStatus();
         assertThat(status).isEqualTo(200);
+    }
+
+    @Test
+    void getToWebsiteReturns200WithRequiredDetails() {
+        ModelAndView modelAndView = mockMvcTester.perform(get("/"))
+                .getMvcResult()
+                .getModelAndView();
+        Map<String, Object> model = Objects.requireNonNull(modelAndView).getModel();
+
+        assertThat(model.get("welcomeMessage")).isEqualTo("Welcome to JBMS");
+        assertThat(model.get("totalBookmarksCount")).isEqualTo(2);
+        assertThat(model.get("totalTagsCount")).isEqualTo(3);
+
+        assertThat(modelAndView.getViewName()).isEqualTo("welcome");
+
     }
 }
