ff57e3cd01c0f8d39546b73aefaee4b73fedde5e
Abhishek <abhi@dev.com>
2025-11-08 23:17:05 +0530

    Just refactored WelcomeControllerMvcTest

--- src/test/java/com/amalvadkar/jbms/adapter/in/web/WelcomeControllerMvcTest.java
+++ src/test/java/com/amalvadkar/jbms/adapter/in/web/WelcomeControllerMvcTest.java
@@ -3,6 +3,7 @@ package com.amalvadkar.jbms.adapter.in.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,25 +20,28 @@ public class WelcomeControllerMvcTest {
     MockMvcTester mockMvcTester;
 
     @Test
-    void getToWebsiteReturns200() {
-        int status = mockMvcTester.perform(get("/"))
-                .getResponse()
-                .getStatus();
+    void getToWebsiteReturns200WithWelcomePage() {
+        MvcResult mvcResult = mockMvcTester.perform(get("/"))
+                .getMvcResult();
+
+        int status = mvcResult.getResponse().getStatus();
         assertThat(status).isEqualTo(200);
+
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        String viewName = Objects.requireNonNull(modelAndView).getViewName();
+        assertThat(viewName).isEqualTo("welcome");
     }
 
     @Test
-    void getToWebsiteReturns200WithRequiredDetails() {
+    void getToWebsiteReturns200WithWelcomePageAlongWithRequiredDetails() {
         ModelAndView modelAndView = mockMvcTester.perform(get("/"))
-                .getMvcResult()
-                .getModelAndView();
-        Map<String, Object> model = Objects.requireNonNull(modelAndView).getModel();
+                .getMvcResult().getModelAndView();
 
+        Map<String, Object> model = Objects.requireNonNull(modelAndView).getModel();
         assertThat(model.get("welcomeMessage")).isEqualTo("Welcome to JBMS");
         assertThat(model.get("totalBookmarksCount")).isEqualTo(2);
         assertThat(model.get("totalTagsCount")).isEqualTo(3);
 
         assertThat(modelAndView.getViewName()).isEqualTo("welcome");
-
     }
 }
