d653829f42637afa1a90de48dbbcecab1145cf69
Abhishek <abhi@dev.com>
2025-11-08 23:46:20 +0530

    Created failing test for get to /bookmarks endpoint then return 200 with bookmarks list page

---
+++ src/test/java/com/amalvadkar/jbms/adapter/in/web/BookmarkControllerMvcTest.java
@@ -0,0 +1,24 @@
+package com.amalvadkar.jbms.adapter.in.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+public class BookmarkControllerMvcTest {
+
+    @Autowired
+    MockMvcTester mockMvcTester;
+
+    @Test
+    void getToBookmarksEndpointReturns200WithBookmarkListPage() {
+        mockMvcTester.perform(get("/bookmarks"))
+                .assertThat()
+                .hasStatus2xxSuccessful()
+                .hasViewName("bookmark-list");
+
+    }
+}
