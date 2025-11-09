6c62bedeed149038f2b3ecd13e8c42c12fb3474f
Abhishek <abhi@dev.com>
2025-11-09 13:05:24 +0530

    Show sample bookmarks on bookmark list page

--- src/main/java/com/amalvadkar/jbms/JbmsConfig.java
+++ src/main/java/com/amalvadkar/jbms/JbmsConfig.java
@@ -4,9 +4,11 @@ import com.amalvadkar.jbms.adapter.out.InMemoryBookmarkRepository;
 import com.amalvadkar.jbms.application.port.BookmarkRepository;
 import com.amalvadkar.jbms.application.port.BookmarkService;
 import com.amalvadkar.jbms.domain.Bookmark;
+import com.amalvadkar.jbms.domain.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Configuration
@@ -14,9 +16,10 @@ public class JbmsConfig {
 
     @Bean
     public BookmarkService bookmarkService(){
-        List<Bookmark> bookmarks = List.of();
-        BookmarkRepository bookMarkRepository = new InMemoryBookmarkRepository(bookmarks);
-        return new BookmarkService(bookMarkRepository);
+        Bookmark bookmarkOne = new Bookmark("Bookmark 1", "Bookmark 1 url", ZonedDateTime.now(), List.of(new Tag("spring"), new Tag("java")));
+        Bookmark bookmarkTwo = new Bookmark("Bookmark 2", "Bookmark 2 url", ZonedDateTime.now(), List.of(new Tag("mysql"), new Tag("ssh")));
+        BookmarkRepository bookmarkRepository = new InMemoryBookmarkRepository(List.of(bookmarkOne, bookmarkTwo));
+        return new BookmarkService(bookmarkRepository);
     }
 
 }
